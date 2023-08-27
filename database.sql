de72f617deaf56da25c1ee27601e3eb948bf7d61
Aubrey Graves <grav0165@gmail.com>
2023-08-27 12:50:58 -0500

    Added SQL database queries. Cleaning up from ERD export.

--- database.sql
+++ database.sql
@@ -6,5 +6,37 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "admin_level" INT
+);
+
+CREATE TABLE 'node' (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT NOT NULL,
+	"node_name" varchar(200) NOT NULL,
+	"catagory" varchar(200) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE 'node_association' (
+	"id" SERIAL PRIMARY KEY,
+	"node_id" INT NOT NULL,
+	"user_id" INT NOT NULL,
+	"auth_code" varchar(8) NOT NULL,
+	PRIMARY KEY ("id")
+);
+
+CREATE TABLE "posts" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT,
+	"content" varchar(10000),
+	"node_id" INT,
+	"orig_post" BOOLEAN DEFAULT false,
+	"reply_id" INT,
+	"post_time" TIMESTAMP,
+	"edit" BOOLEAN DEFAULT false,
+	"public" BOOLEAN DEFAULT false,
+	"reported" BOOLEAN DEFAULT false,
+	"votes" INT,
+	PRIMARY KEY ("id")
 );
