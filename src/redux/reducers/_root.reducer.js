459070ba7aeeffd69d5b8a0e542197442f7e4e38
Aubrey Graves <grav0165@gmail.com>
2023-08-27 13:15:08 -0500

    Created node reducer. Added Node saga and reducer to the root saga and root reducers

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
+import nodeReducer from './node.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -11,6 +12,7 @@ import user from './user.reducer';
 const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
+  nodeReducer, // holds all of currently available nodes
 });
 
 export default rootReducer;
