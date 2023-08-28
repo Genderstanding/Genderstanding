3e6a423d2e6ec76dc765fc4981007b3a1b52f02f
Aubrey Graves <grav0165@gmail.com>
2023-08-28 15:01:59 -0500

    fixing merge conflicts

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import nodeReducer from './node.reducer';
 import postReducer from './post.reducer';
+import nodeAssociationReducer from './nodeAssociation.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,6 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   nodeReducer, // holds all of currently available nodes
   postReducer, // holds all posts in all nodes
+  nodeAssociationReducer,
 });
 
 export default rootReducer;
