611b65b4d0595cda1b5b2c0ec97d0452a18fdf0a
Quynh Ngan Dao <nguy2884@umn.edu>
2023-08-30 16:12:56 -0500

    fixing issues with navbar

--- src/components/FeaturedPage/FeaturedPage.jsx
+++ src/components/FeaturedPage/FeaturedPage.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './FeaturedPage.css'
+import HeaderBar from "../HeaderBar/HeaderBar";
 
 export default function FeaturedPage() {
-  return (
-    <div>Featured Page</div>
+  return (<>
+     <div className="flex flex-col h-screen App">
+     <HeaderBar/>
+     <div className='mt-4 featured-container'>
+            <h2>Featured</h2>
+            <h4>View community nodes</h4>
+            <div className='mt-2 ml-8 featured-buttons'>
+              <button className='mr-4 underline'>Trending</button>
+              <button className='mr-4 underline'>Latest</button>
+              <button className='mr-4 underline'>Popular</button>
+            </div>
+            <div className='flex flex-col items-center featured-nodes-homepage'>
+              <div className='featured-top'>
+                  Featured Spotlight text 1
+              </div>
+              <div className='featured-bottom'>
+                  Featured Spotlight text 2
+              </div>
+            </div>
+          </div>  
+
+        </div>
+   
+    </>
   )
 }
 
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,86 +1,50 @@
-import React, { useState } from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import { useSelector, useDispatch } from 'react-redux';
-import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
-import AddNodeModal from '../AddNodeModal/AddNodeModal';
-import SettingsModal from '../SettingsModal/SettingsModal';
-import { useHistory } from 'react-router-dom';
-import './UserPage.css'
-
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useHistory } from "react-router-dom";
+import "./UserPage.css";
+import HeaderBar from "../HeaderBar/HeaderBar";
 
 function UserPage() {
-  const [addNodeOpen, setaddNodeOpen] = useState(false);
-  const [settingsOpen, setSettingsOpen] = useState(false);
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   const history = useHistory();
   // sourcing dispatch to use calls
   const dispatch = useDispatch();
   // sourcing use selector to hold store information
-  let newNode = useSelector(store => store.newNodeReducer.newNodeDatabaseResponse)
-
-
-  const openAddNode = () => {
-    setaddNodeOpen(true);
-  };
-
-  const closeAddNode = () => {
-    setaddNodeOpen(false);
-  };
-
-  const openSettings = () => {
-    setSettingsOpen(true);
-  }
-
-  const closeSettings = () => {
-    setSettingsOpen(false);
-  }
+  let newNode = useSelector(
+    (store) => store.newNodeReducer.newNodeDatabaseResponse
+  );
 
   const goToOwnerNodes = () => {
-    history.push('/owner');
+    history.push("/owner");
   };
 
   const goToUserNodes = () => {
-    history.push('/usernodes');
+    history.push("/usernodes");
   };
 
   return (
+    <div className="flex flex-col h-screen userpage-container">
+      <HeaderBar />
 
-    <div className="userpage-container flex flex-col h-screen">
-      <div className='header-container flex items-center '>
-        <MdChevronLeft size={25} className='ml-2' />
-        {/* this flex-grow div is tailwind way to spread out the back and add buttons*/}
-        <div className="flex-grow"></div>
-        <button className="mr-4 text-2xl" onClick={openAddNode}>+</button>
-      </div>
-
-      <div className='userpage-boxes flex flex-col justify-center'>
-
-        <h2 className='ml-5 mt-4 mb-1'>Communities you created:</h2>
-        <div className='moderator-box flex items-center justify-center mb-4'>
+      <div className="flex flex-col justify-center userpage-boxes">
+        <h2 className="mt-4 mb-1 ml-5">Communities you created:</h2>
+        <div className="flex items-center justify-center mb-4 moderator-box">
           {/* map communities you moderate inside these divs*/}
           <div className="moderator-container" onClick={goToOwnerNodes}>
-            <div className='owned-community-names m-4'>
-              What's New?
-            </div>
+            <div className="m-4 owned-community-names">What's New?</div>
           </div>
         </div>
 
-        <h2 className='ml-5 mt-4 mb-1'>Communities you're a part of:</h2>
-        <div className='part-of-box flex items-center justify-center mb-4'>
+        <h2 className="mt-4 mb-1 ml-5">Communities you're a part of:</h2>
+        <div className="flex items-center justify-center mb-4 part-of-box">
           {/* map communities you particpate in in this div*/}
-          <div className='user-container' onClick={goToUserNodes}>
-            <div className='user-community-names m-4'>
-              Why not cheese?
-            </div>
+          <div className="user-container" onClick={goToUserNodes}>
+            <div className="m-4 user-community-names">Why not cheese?</div>
           </div>
         </div>
       </div>
-
-
-
-      <AddNodeModal addNodeOpen={addNodeOpen} addNodeClose={closeAddNode} />
-      <SettingsModal settingsOpen={settingsOpen} closeSettings={closeSettings} />
     </div>
   );
 }
