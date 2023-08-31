65120f093b67eac4824412bd1b15f6e95d7bd885
Justin Lawson <jstn.lawson@gmail.com>
2023-08-31 13:54:35 -0500

    added copy code function and dummy code

--- src/components/InviteNodeModal/InviteNodeModal.jsx
+++ src/components/InviteNodeModal/InviteNodeModal.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import './InviteNodeModal.css'
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Popover from "@mui/material/Popover";
 
 
 export const InviteNodeModal = ({
@@ -18,12 +17,23 @@ export const InviteNodeModal = ({
   // sourcing use selector to hold store information
   let newCode = useSelector((store) => store.newCodeReducer);
 
+  const [codeText, setCodeText] = useState('1a2b3c4d');
+  const [isCodeCopied, setIsCodeCopied] = useState(false);
 
+  const copyCode = async () => {
+    try {
+      await navigator.clipboard.writeText(codeText);
+      setIsCodeCopied(true); 
+    } catch (error) {
+      console.error("Error copying code:", error);
+    }
+  };
 
   // function to handle posting and getting code from database
   const handleGenerateCode = () => {
     try {
       dispatch({ type: "GENERATE_CODE" });
+      setIsCodeCopied(false);
     } catch (error) {
       console.log("Error in button click to generate new node: ", error);
     }
@@ -36,9 +46,9 @@ export const InviteNodeModal = ({
         {children}
         <h2 className="mb-4 mr-4 text-xl font-bold">Generate Invite Code</h2>
         <div className="code-container">
-          <span>1a2b3c4d</span>
-          <button className="copy-code-button underline ml-4">
-            copy
+          <span className="code-text">{codeText}</span>
+          <button className="copy-code-button ml-4" onClick={copyCode}>
+          {isCodeCopied ? "Code Copied!" : "Copy Code"}
           </button>
         </div>
         <div className="buttons-container flex mt-6">
