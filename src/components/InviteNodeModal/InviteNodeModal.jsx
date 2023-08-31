a7e73715bf7e69ffdb3a663ecde8bf62ee7ab1c7
Justin Lawson <jstn.lawson@gmail.com>
2023-08-31 12:17:01 -0500

    fixed import on invite modal

--- src/components/InviteNodeModal/InviteNodeModal.jsx
+++ src/components/InviteNodeModal/InviteNodeModal.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import '../InviteNodeModal/InviteNodeModal.css'
+import './InviteNodeModal.css'
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import Popover from "@mui/material/Popover";
+
 
 export const InviteNodeModal = ({
   InviteCodeOpen,
@@ -16,6 +18,8 @@ export const InviteNodeModal = ({
   // sourcing use selector to hold store information
   let newCode = useSelector((store) => store.newCodeReducer);
 
+
+
   // function to handle posting and getting code from database
   const handleGenerateCode = () => {
     try {
@@ -24,13 +28,20 @@ export const InviteNodeModal = ({
       console.log("Error in button click to generate new node: ", error);
     }
   };
+  
 
   return (
     <div className="flex items-center justify-center modal-overlay">
       <div className="flex flex-col items-center justify-center invite-code-modal">
         {children}
         <h2 className="mb-4 mr-4 text-xl font-bold">Generate Invite Code</h2>
-        <div className="mt-6 buttons-container">
+        <div className="code-container">
+          <span>1a2b3c4d</span>
+          <button className="copy-code-button underline ml-4">
+            copy
+          </button>
+        </div>
+        <div className="buttons-container flex mt-6">
           <button className="mr-6 underline" onClick={handleGenerateCode}>
             Generate
           </button>
