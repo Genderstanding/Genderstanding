8765f0ef99d8827e0cc7824f0b074f57932c64ea
Justin Lawson <jstn.lawson@gmail.com>
2023-08-31 21:07:51 -0500

    editing test in state but it is not great

---
+++ src/components/ElipsisModal/ElipsisModal.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from 'react';
+import './ElipsisModal.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+
+
+
+const ElipsisModal = ({ elipsisOpen, elipsisClose, contentToEdit, handleSaveEdit }) => {
+    const [editedContent, setEditedContent] = useState(contentToEdit);
+    const [isEditing, setIsEditing] = useState(false);
+
+    const handleEditToggle = () => {
+        setIsEditing(!isEditing);
+    };
+
+    const handleSave = () => {
+        handleSaveEdit(editedContent); // Call the parent's handleSaveEdit function
+        setIsEditing(false);
+    };
+
+    if (!elipsisOpen) {
+        return null;
+    }
+
+
+    return (
+        <div className='flex items-center justify-center modal-overlay'>
+            <div className='flex flex-col items-center justify-center add-node-modal'>
+                {/* {children} */}
+                <div className=' flex flex-col justify-start buttons-container'>
+
+                    {/* <button className='underline m-2'>
+                                Edit
+                            </button>
+                            <button className='underline m-2'>
+                                Delete
+                            </button>
+                            <button className='underline m-2'>
+                                Remove User
+                            </button>
+                            <button className='underline m-2'>
+                                Report User
+                            </button> */}
+
+                    {isEditing ? (
+                        <textarea
+                            value={editedContent}
+                            onChange={(e) => setEditedContent(e.target.value)}
+                            className='edit-textarea'
+                        />
+                    ) : (
+                        <div className='m-4 question-text'>{editedContent}</div>
+                    )}
+                    <div className='flex flex-col justify-start buttons-container'>
+                        {isEditing ? (
+                            <button className='underline m-2' onClick={handleSave}>
+                                Save
+                            </button>
+                        ) : (
+                            <>
+                                <button className='underline m-2' onClick={handleEditToggle}>
+                                    Edit
+                                </button>
+                                <button className='underline m-2'>Delete</button>
+                                <button className='underline m-2'>Remove User</button>
+                                <button className='underline m-2'>Report User</button>
+                            </>
+                        )}
+
+                        <button className='underline m-2' onClick={elipsisClose}>
+                            Close
+                        </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            );
+};
+
+export default ElipsisModal;
