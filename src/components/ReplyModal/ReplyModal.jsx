9721bf7b952fb2cfff3fca9fa14de1e544c1c724
Aubrey Graves <grav0165@gmail.com>
2023-09-01 15:20:34 -0500

    you can post replies

--- src/components/ReplyModal/ReplyModal.jsx
+++ src/components/ReplyModal/ReplyModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './ReplyModal.css'
-import { useSelector, useDispatch} from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 
 const ReplyModal = ({ addReplyOpen, closeAddReply, questionObject }) => {
@@ -9,26 +9,32 @@ const ReplyModal = ({ addReplyOpen, closeAddReply, questionObject }) => {
     }
     const dispatch = useDispatch();
     let nodePosts = useSelector(store => store.postReducer.postDatabaseResponse)
+    const reversePosts = nodePosts.slice().reverse();
 
     // Creating a state to hold text inputed
     const [replyInput, setReplyInput] = useState('');
 
+    console.log('reply input is: ', reversePosts)
+
+
     const handleReply = (event, questionObject) => {
         event.preventDefault();
-        try{
-            dispatch({ 
-                type: 'CREATE_REPLY', 
+        try {
+            dispatch({
+                type: 'CREATE_POST',
                 payload: {
                     content: replyInput,
-                    reply_id: questionObject.id
+                    reply_id: questionObject.id,
+                    node_id: questionObject.node_id,
+                    orig_post: false
                 }
-                })
+            })
         } catch (error) {
             console.log('Error in button click to create new reply: ', error)
         }
 
     }
-console.log('question object is: ', questionObject)
+    console.log('question object is: ', questionObject)
 
     return (
         <div className='flex items-center justify-center modal-overlay'>
@@ -39,25 +45,24 @@ console.log('question object is: ', questionObject)
                     {questionObject.content}
                 </div>
 
-                {nodePosts.map(post => {
+                {reversePosts.map(post => {
                     if (post.reply_id == questionObject.id) {
                         return (
-                            <div className="question-text m-4">
+                            <div className="question-text m-4" key={post.id}>
                                 {post.content}
                             </div>
                         )
                     }
                 })}
-
                 <textarea
                     rows="4"
                     className='w-full px-4 py-2 text-sm text-gray-900 bg-white border-0 reply-textarea dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400'
-                    placeholder="Write a Reply..." 
-                    onChange={(event)=>setReplyInput(event.target.input)}
+                    placeholder="Write a Reply..."
+                    onChange={(event) => setReplyInput(event.target.value)}
                     required>
-                 </textarea>
+                </textarea>
                 <div className='mt-6 buttons-container'>
-                    <button className='mr-6 underline' onClick={handleReply}>Confirm</button>
+                    <button className='mr-6 underline' onClick={(event) => handleReply(event, questionObject)}>Confirm</button>
 
                     <button className='underline ' onClick={closeAddReply}>
                         Close
