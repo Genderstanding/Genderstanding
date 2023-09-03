6abc23bbd43bd1f70ba6e0fef3b3b9aa2e357fe0
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-03 13:35:16 -0500

    working on dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
-  darkMode: "class",
   theme: {
     extend: {
       colors: {
         primary: "#CF6F5A", 
-        secondary: "#D68978",
+        secondary: "#ECC5B1",
+        background: "#FBF1EE",
+        text: "#080302",
+        accent: {
+          1: "", 
+          2: "",
+        }, 
+        bkg: "",
+        userContent:"",
+        ownerContent:"",
       },
     },
   },
