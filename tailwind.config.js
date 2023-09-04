fb016722be77a76fab527d6262f00b81bf31ef49
Quynh Ngan Dao <nguy2884@umn.edu>
2023-09-04 00:06:43 -0500

    working with dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        primary: "#CF6F5A", 
-        secondary: "#ECC5B1",
-        background: "#FBF1EE",
-        text: "#080302",
-        accent: {
-          1: "", 
-          2: "",
-        }, 
-        bkg: "",
-        userContent:"",
-        ownerContent:"",
-      },
+        primary: "rgb(var(--colors-primary))", 
+        secondary:  "rgb(var(--colors-secondary))", 
+      //   accent: "rgb(var(--color-accent) / <alpha-value>)",
+      //   bkg: "rgb(var(--color-bkg) / <alpha-value>)",
+      //   text: "rgb(var(--color-text) / <alpha-value>)",
+      // },
+      // userContent: "rgb(var(--userContent) / <alpha-value>)",
+      // ownerContent: "rgb(var(--ownerContent) / <alpha-value>)",
     },
+  }
   },
-  variants: {},
   plugins: [],
 };
