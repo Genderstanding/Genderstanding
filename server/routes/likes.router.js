08e1059a671ad2578895059f62591f511c8061d0
Aubrey Graves <grav0165@gmail.com>
2023-09-06 12:21:45 -0500

    created like saga, like reducer, like router

---
+++ server/routes/likes.router.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const pool = require('../modules/pool')
+const axios = require('axios');
+const { rejectUnauthenticated } = require('../modules/authentication-middleware')
+
+// creating the router name for likes
+const likesRouter = express.Router();
+
+// GET route for all likes made by user
+likesRouter.get('/', rejectUnauthenticated, (req, res) => {
+    let sqlValue = req.user.id; 
+    let sqlQuery = `
+    
+    `
+    pool.query(sqlQuery, [ sqlValue ])
+    .then(result => {
+        res.send(result.rows);
+    })
+    .catch(error => {
+        console.log('Error in GET of likes: ', error);
+        res.sendStatus(500);
+    })
+})
+
+likesRouter.post('/', rejectUnauthenticated, (req, res) => {
+    let sqlUserId = req.user.id;
+    let sqlPostId = req.body;
+    let sqlQuery = `
+    INSERT INTO "likes" ("user_id", "post_id")
+    VALUES ($1, $2);
+    `;
+    pool.query(sqlQuery, [sqlUserId, sqlPostId])
+    .then(result => {
+        console.log('Created a new like in database: ', result);
+        res.sendStatus(201);
+    })
+    .catch(error => {
+        console.log('Error in router POST to likes: ', error);
+        res.sendStatus(500)
+    })
+})
+
+module.exports = likesRouter
---
+++ src/redux/sagas/likes.saga.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import { put, takeLatest } from "redux-saga/effects"
+
+function* fetchLikes(){
+    try{
+        const likeResponse = yield axios.get('/likes')
+        yield put({ type: 'SET_LIKES', payload: likeResponse.data})
+    } catch(error) {
+        console.log('Error in SAGA GET of likes: ', error)
+    }
+}
+
+function* likePost(action){
+    try{
+        yield axios.post('/likes', action.payload)
+        yield put({ type: 'FETCH_LIKES' })
+    } catch (error) {
+        console.log('Error in SAGA POST for like: ', error)
+    }
+}
+
+
+function* likeSaga(){
+    yield takeLatest('FETCH_LIKES', fetchLikes);
+    yield takeLatest('LIKE_POST', likePost);
+}
+
+export default likeSaga;
